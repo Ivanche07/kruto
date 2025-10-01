2e680aba9b4bf6c8bfd5802b81034effd623dc44
Seredin Nikolay <kolyaseredin128@gmail.com>
2025-10-01 17:28:59 +0300

    First task

---
+++ get-dac/8-bit-dac-manual.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as g 
+import time as t 
+dac_bits = [16,12,25,17,27,23,22,24]
+g.setmode(g.BCM)
+g.setup(dac_bits, g.OUT)
+dynamic_range = 3.3
+def voltage_to_number(voltage):
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
+        print("Устанавлниваем 0.0 В")
+        return 0
+    return int(voltage / dynamic_range * 255)
+def number_to_dac(number):
+    number = [int(element) for element in bin(number)[2:].zfill(8)]
+def f(number, dac_bits):
+    for i in len(number):
+        if number[i]:
+            g.output(dac_bits[number[i]],1)
+try:
+    while True:
+        try:
+            voltage = float(input("Введите напряжение в Вольтах: "))
+            number = voltage_to_number(voltage)
+            number_to_dac(number)
+            f(number)
+
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз\n")
+
+finally:
+    g.output(dac_bits, 0)
+    g.cleanup()
